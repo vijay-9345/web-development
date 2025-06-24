38f2112320a5109a5a6da1a6ff74c24aaccdecc3
vijay-9345 <vijay824851@gmail.com>
2025-06-24 08:56:14 +0530

    day6

---
+++ Backend/model/user.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+
+const userSchema=new mongoose.Schema({
+    name:{type:String,required:true},
+    age:Number
+})
+const User=mongoose.model('users',userSchema)
+export default User
